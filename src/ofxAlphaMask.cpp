1fc6ebe76358b107974e26dcedbe13ca1aa1142a
M-wataru-kani <wataru-kani@aquaring.co.jp>
2017-10-13 12:41:41 +0900

    メンバをstaticに変更

--- src/ofxAlphaMask.cpp
+++ src/ofxAlphaMask.cpp
@@ -1,1 +1,44 @@
 #include "ofxAlphaMask.h"
+
+ofShader ofxAlphaMask::shader;
+
+string   ofxAlphaMask::fragSrc = R"(
+#version 120
+#extension GL_ARB_texture_rectangle : enable
+
+uniform sampler2DRect tex0;
+uniform sampler2DRect maskTex;
+
+void main( void ) {
+  vec2  pos    = gl_TexCoord[ 0 ].st;
+  vec3  src    = texture2DRect( tex0, pos ).rgb;
+  float mask   = texture2DRect( maskTex, pos ).r;
+  gl_FragColor = vec4( src, mask );
+}
+)";
+
+bool ofxAlphaMask::bLoaded = false;
+
+
+//--------------------------------------------------------------
+ofxAlphaMask::ofxAlphaMask()
+{
+  if( ofxAlphaMask::bLoaded ) return;
+  
+  ofxAlphaMask::shader.setupShaderFromSource( GL_FRAGMENT_SHADER, fragSrc );
+  ofxAlphaMask::shader.linkProgram();
+  ofxAlphaMask::bLoaded = true;
+}
+
+//--------------------------------------------------------------
+void ofxAlphaMask::begin( ofTexture& _tex )
+{
+  ofxAlphaMask::shader.begin();
+  ofxAlphaMask::shader.setUniformTexture( "maskTex", _tex, 1 );
+}
+
+//--------------------------------------------------------------
+void ofxAlphaMask::end()
+{
+  ofxAlphaMask::shader.end();
+}
--- src/ofxAlphaMask.h
+++ src/ofxAlphaMask.h
@@ -5,38 +5,13 @@
 
 class ofxAlphaMask
 {
-  ofShader shader;
+  static ofShader shader;
+  static string   fragSrc;
+  static bool     bLoaded;
   
 public:
-  ofxAlphaMask()
-  {
-    string fragSrc = R"(
-#version 120
-#extension GL_ARB_texture_rectangle : enable
-
-uniform sampler2DRect tex0;
-uniform sampler2DRect maskTex;
-
-void main( void ) {
-  vec2  pos    = gl_TexCoord[ 0 ].st;
-  vec3  src    = texture2DRect( tex0, pos ).rgb;
-  float mask   = texture2DRect( maskTex, pos ).r;
-  gl_FragColor = vec4( src, mask );
-}
-    )";
-    
-    shader.setupShaderFromSource( GL_FRAGMENT_SHADER, fragSrc );
-    shader.linkProgram();
-  }
+  ofxAlphaMask();
   
-  void begin( ofTexture& _tex )
-  {
-    shader.begin();
-    shader.setUniformTexture( "maskTex", _tex, 1 );
-  }
-  
-  void end()
-  {
-    shader.end();
-  }
+  void begin( ofTexture& _tex );
+  void end();
 };
